dab5f61ba49d3ec7a4c781f91c25968c7dcc98c5
rustamin <rustamjr_11@yahoo.com>
2016-03-29 12:01:45 +0700

    udah bisa views nya

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var routes = require('./routes/index');
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
